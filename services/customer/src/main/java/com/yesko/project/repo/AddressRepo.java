4458b48b6707550dc1cb6af0d004e1309e71da3e
yessetassan <139701904+yessetassan@users.noreply.github.com>
2024-08-04 23:58:06 +0500

    added swagger on customer-service...

--- services/customer/src/main/java/com/yesko/project/repo/AddressRepo.java
+++
@@ -1,9 +0,0 @@
-package com.yesko.project.repo;
-
-import com.yesko.project.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddressRepo extends JpaRepository<Address, Integer> {
-}
